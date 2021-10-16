2ee81175e34907338e8c4e3cdb8a5472ccbb98b7
uchihamalolann <uchihamalolan@gmail.com>
2021-10-16 09:59:19 +0530

    adds dvgr and editor config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "@honkhonk/vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths()],
+  plugins: [reactRefresh(), tsconfigPaths(), svgr()],
 });
