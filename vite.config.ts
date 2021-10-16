fd50e8269fe9956e87eabd8b16ea46f31c0caa4c
uchihamalolann <uchihamalolan@gmail.com>
2021-10-16 11:03:40 +0530

    updates vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "@honkhonk/vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths(), svgr()],
+  plugins: [react(), tsconfigPaths(), svgr()],
 });
