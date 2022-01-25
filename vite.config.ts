8d18842aa3657fc0ab0a58b41886e2eafdc227b2
uchihamalolann <uchihamalolan@gmail.com>
2022-01-25 19:27:24 +0530

    chore: eslint fix for vscode

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
+// eslint-disable-next-line import/no-unused-modules
 export default defineConfig({
   envDir: './env',
   plugins: [react(), tsconfigPaths()],
