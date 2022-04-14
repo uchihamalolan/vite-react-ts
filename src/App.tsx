3b6969adf53adae448e173a3b79dbb5db5933a3e
Malolan <malolan@growfin.ai>
2022-04-14 20:11:30 +0530

    feat(global): adds svgr pluigin to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from 'components/Header';
 import { Button } from 'components/Button';
+import { ReactComponent as Logo } from 'assets/favicon.svg';
 
 function App() {
   return (
     <div className="App">
       <Header title="hola" />
+      <Logo height={100} width={100} />
       <Button onClick={() => alert('hola')}>Heyo</Button>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 // eslint-disable-next-line import/no-unused-modules
 export default defineConfig({
   envDir: './env',
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), svgrPlugin()],
   /* If proxy is needed
   server: {
     proxy: {
