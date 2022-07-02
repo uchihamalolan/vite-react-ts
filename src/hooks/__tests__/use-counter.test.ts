1ad97c6ac4fe4310dc65c7367f33b258f687a803
amjed-ali-k <amjedmgm@gmail.com>
2022-07-02 10:23:59 +0400

    fix: renderHook supported react-testing-library iteself

--- src/hooks/__tests__/use-counter.test.ts
+++ src/hooks/__tests__/use-counter.test.ts
@@ -1,9 +1,11 @@
-import { renderHook } from '@testing-library/react-hooks';
+/**
+ * @jest-environment jsdom
+ */
+import { renderHook } from '@testing-library/react';
 import useCounter from '../use-counter';
 
 test('should use counter', () => {
   const { result } = renderHook(() => useCounter());
-
   expect(result.current.count).toBe(0);
   expect(typeof result.current.increment).toBe('function');
 });
